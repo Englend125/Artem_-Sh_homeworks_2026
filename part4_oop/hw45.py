14cdad9b6740a3315b0a703a67cf0aef514fff7b
noobmaster_2007 <yshkin2007@gmail.com>
2026-05-25 07:50:26 +0300

    Reformat hw45.py

--- part4_oop/hw45.py
+++ part4_oop/hw45.py
@@ -7,6 +7,7 @@ from part4_oop.interfaces import Cache, HasCache, Policy, Storage
 K = TypeVar("K")
 V = TypeVar("V")
 
+
 @dataclass
 class DictStorage(Storage[K, V]):
     _data: dict[K, V] = field(default_factory=dict, init=False)
